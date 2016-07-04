32df8610303fc8ab2577e1cdcddbdda7828acd1b
fishlyliu <106899051@qq.com>
2016-07-04 14:48:07 +0800

    Node.js Server SDK V1.0

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = { 
+    sdkAppid: '1400001111',
+    identifier: 'admin',
+    accountType:  '111',
+    version: '201512300000',
+    privateKey : '/path/private.pem',
+    expireAfter: 30 * 24 * 3600,
+};
+
